57eb965cab86a41265b471d1311171cb71c8d01a
MicahY21 <Micahyoung2001@gmail.com>
2023-10-04 22:07:44 -0700

    Unit3LessonB_Done2

--- Assets/Scripts/MoveLeft.cs
+++ Assets/Scripts/MoveLeft.cs
@@ -6,7 +6,7 @@ public class MoveLeft : MonoBehaviour
 {
     public float speed;
     private PlayerController playerCtrl;
-    private float leftBounds = 10;
+    private float leftBounds = -10;
     // Start is called before the first frame update
     void Start()
     {
